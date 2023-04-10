37fea943bdd7e82172899c5129d207e9e899ed2d
davMac606 <cl201238@g.unicamp.br>
2023-04-10 20:59:51 -0300

    didn't work, went to the good old 'gambiarra'

--- echo.php
+++ echo.php
@@ -7,6 +7,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Echo</title>
   <link rel="stylesheet" type="text/css" href="bootstrap-5.2.2-dist\css\bootstrap.min.css">
+  <style>
+    .myContainer {
+      background-color: #aca4a4;
+    }
+    </style>
 </head>
 
 <body>
